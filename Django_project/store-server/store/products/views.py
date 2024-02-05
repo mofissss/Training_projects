326fb9803f2402220134e7a9e5b1a7d9b075598e
Dmitry Kategov <dmkategov@gmail.com>
2024-02-05 23:41:39 +0700

    added db

--- Django_project/store-server/store/products/views.py
+++ Django_project/store-server/store/products/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import Product, ProductCategory
 
 def index(request):
     context = {
-        'title': 'Store',
-        'is_promotion': True,
+        'title': 'Store'
     }
     return render(request, 'products/index.html', context)
 
 def products(request):
     context = {
         'title': 'Store - каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)

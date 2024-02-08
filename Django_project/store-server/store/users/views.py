bdfa8e96ac0fa4775b0fd5df00795506d9fbe954
Dmitry Kategov <dmkategov@gmail.com>
2024-02-08 17:46:59 +0700

    added profile

--- Django_project/store-server/store/users/views.py
+++ Django_project/store-server/store/users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -23,15 +23,37 @@ def login(request):
     }
     return render(request, 'users/login.html', context)
 
+
 def registration(request):
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('index'))
+            messages.success(request, 'Поздравяем! Вы успешно зарегистрировались!')
+            return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
     context = {
         'form': form,
     }
     return render(request, 'users/register.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'title': 'Store - Профиль',
+        'form': form
+    }
+    return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))

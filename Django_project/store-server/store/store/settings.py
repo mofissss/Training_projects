074a291b60848327c1ebc71118dc1fc6ff075c11
Dmitry Kategov <dmkategov@gmail.com>
2024-02-21 14:08:15 +0700

    added postgresql

--- Django_project/store-server/store/store/settings.py
+++ Django_project/store-server/store/store/settings.py
@@ -84,9 +84,13 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "store_db",
+        "USER": os.getenv("USER_DB"),
+        "PASSWORD": os.getenv("PASSWORD_DB"),
+        "HOST": "localhost",
+        "PORT": 5432
     }
 }
 
